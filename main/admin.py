e67fa133ada0bd4767af8aee7b6dda2b1b317b70
coplant <omg.dayz@gmail.com>
2022-11-12 02:38:26 +0700

    12.11.22 - added models.py & admin.py

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,43 @@
 from django.contrib import admin
+from .models import Review, User, Schedule, Subscription, Direction
 
-# Register your models here.
+
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ('id', 'author', 'content', 'date')
+    list_display_links = ('id',)
+    search_fields = ('author',)
+
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'username', 'first_name', 'last_name', 'is_coach', 'is_active', 'date_joined')
+    list_display_links = ('id', 'username')
+    search_fields = ('username',)
+    list_editable = ('is_active', 'is_coach')
+
+
+class ScheduleAdmin(admin.ModelAdmin):
+    list_display = ('id', 'direction', 'date')
+    list_display_links = ('id',)
+    search_fields = ('direction', 'date')
+    list_editable = ('date',)
+    list_filter = ('direction', 'date')
+
+
+class SubscriptionAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'price')
+    list_display_links = ('id', 'title')
+    search_fields = ('title', 'price')
+    list_editable = ('price',)
+
+
+class DirectionAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title')
+    list_display_links = ('id', 'title')
+    search_fields = ('title',)
+
+
+admin.site.register(Review, ReviewAdmin)
+admin.site.register(User, UserAdmin)
+admin.site.register(Schedule, ScheduleAdmin)
+admin.site.register(Direction, DirectionAdmin)
+admin.site.register(Subscription, SubscriptionAdmin)
--- main/models.py
+++ main/models.py
@@ -1,3 +1,69 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.urls import reverse
 
-# Create your models here.
+
+class User(AbstractUser):
+    photo = models.ImageField(upload_to='user/%Y/%m/%d/', blank=True, verbose_name='Фото')
+    is_coach = models.BooleanField(default=False, verbose_name='Статус тренера')
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
+        ordering = ['username']
+
+
+class Review(models.Model):
+    author = models.ForeignKey('User', on_delete=models.CASCADE, verbose_name='Автор')
+    date = models.DateTimeField(auto_now_add=True, verbose_name='Дата публикации')
+    content = models.TextField(verbose_name='Содержание')
+
+    def __str__(self):
+        return f'{self.author.first_name} {self.author.last_name}'
+
+    class Meta:
+        verbose_name = "Отзыв"
+        verbose_name_plural = "Отзывы"
+        ordering = ['date']
+
+
+class Subscription(models.Model):
+    title = models.CharField(max_length=150, verbose_name='Наименование')
+    content = models.TextField(verbose_name='Содержание')
+    price = models.IntegerField(verbose_name='Стоимость')
+    photo = models.ImageField(upload_to='subscription/%Y/%m/%d/', blank=True, verbose_name='Фото')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "Абонемент"
+        verbose_name_plural = "Абонементы"
+
+
+class Direction(models.Model):
+    title = models.CharField(max_length=150, verbose_name='Направление')
+    content = models.TextField(verbose_name='Содержание')
+    photo = models.ImageField(upload_to='direction/%Y/%m/%d/', blank=True, verbose_name='Фото')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "Направление"
+        verbose_name_plural = "Направления"
+
+
+class Schedule(models.Model):
+    direction = models.ForeignKey('Direction', on_delete=models.CASCADE, verbose_name='Направление')
+    date = models.DateTimeField(verbose_name='Дата и время')
+
+    def __str__(self):
+        return self.direction.title
+
+    class Meta:
+        verbose_name = "Расписание"
+        verbose_name_plural = "Расписание"
